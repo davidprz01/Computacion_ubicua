839d5df131f0068f7c8084611413f2af1364edd2
David <148596141+davidprz01@users.noreply.github.com>
2026-02-17 19:53:33 +0000

    m

---
+++ Lab2/capitulo2.ino
@@ -0,0 +1,21 @@
+int PULSADOR = 2; 
+int LED=3; 
+int ESTADO=LOW; 
+
+void setup(){ 
+
+pinMode(PULSADOR, INPUT); 
+pinMode(LED, OUTPUT); 
+digitalWrite(LED, LOW);
+} 
+
+void loop (){ 
+while(digitalRead(PULSADOR) == LOW){ 	
+ }
+
+ESTADO = digitalRead(LED); 		
+digitalWrite(LED, !ESTADO); 		
+
+while(digitalRead(PULSADOR) == HIGH){ 	
+ }
+}
